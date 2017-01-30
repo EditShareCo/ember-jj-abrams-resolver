f260c3812a9d4ea37c3c8f32f99a22e60e53935f
Robert Jackson <rjackson@linkedin.com>
2017-01-29 22:07:41 -0500

    Include `@glimmer/resolver` in `treeForAddon`.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
 var VersionChecker = require('ember-cli-version-checker');
 
 module.exports = {
@@ -18,6 +19,41 @@ module.exports = {
 
     this.app.import('vendor/ember-resolver/legacy-shims.js');
   },
+
+  treeForAddon: function() {
+    var Funnel = require('broccoli-funnel');
+    var MergeTrees = require('broccoli-merge-trees');
+    var Babel = require('broccoli-babel-transpiler');
+    var addonTree = this._super.treeForAddon.apply(this, arguments);
+    var glimmerResolverSrc = require.resolve('@glimmer/resolver/package');
+
+    var glimmerResolverTree = new Funnel(path.dirname(glimmerResolverSrc), {
+      srcDir: 'dist/modules/es2017',
+      destDir: '@glimmer/resolver'
+    });
+
+    // ember-cli@2.12 includes specific babel options to be used for transpilation
+    // if this.options.babel is present, use it
+    // otherwise use the default transpilation options from ember-cli < 2.12
+    var babelOptions;
+    if (this.options && this.options.babel) {
+      babelOptions = this.options.babel;
+    } else {
+      babelOptions = {
+        loose: true,
+        blacklist: ['es6.modules']
+      };
+    }
+
+    glimmerResolverTree = new Babel(glimmerResolverTree, babelOptions);
+
+    var modulesGlimmerResolver = new Funnel(glimmerResolverTree, {
+      destDir: 'modules'
+    });
+
+    return new MergeTrees([addonTree, modulesGlimmerResolver]);
+  },
+
 
   monkeyPatchVendorFiles: function() {
     var filesToAppend = this.app.legacyFilesToAppend;
